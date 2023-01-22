d0f0a7064f8de893655a342a27e159d4afcd3164
Eric Zhou <zhoueric84@gmail.com>
2023-01-21 20:03:33 -0800

    Build example webapp

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users")
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.CONNECTION_STRING);
+
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  })
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+  
+  res.json(user);
+})
+
+// tells API to start
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,72 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000/message")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+    Axios.get("http://localhost:8000/getUsers")
+      .then((response) => {
+        setListOfUsers(response.data);
+      });
   }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:8000/createUser", {
+      name, 
+      age, 
+      username,
+    }).then((response) => {
+        setListOfUsers([
+          ...listOfUsers,
+          {
+            name,
+            age,
+            username,
+          },
+        ]);
+      });
+  };
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <input 
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+        }}/>
+        <input 
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+        }}/>
+        <input 
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+        }}/>
+        <button onClick={createUser}>Create User</button>
+      </div>
     </div>
   );
 }
