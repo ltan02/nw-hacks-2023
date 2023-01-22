d7fd0a4caf403cf2c061c59e78b83f3e62e2a74f
nikkilaude <nikkilaudee@gmail.com>
2023-01-22 01:29:30 -0800

    Set up initial login page

---
+++ frontend/src/components/authentication/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <div
+        className="absolute w-[575px] h-[108px] left-[468px] top-[132px] 
+        font-medium text-8xl leading-[120px] text-center capitalize text-[#1B1C57]"
+      >
+        Hello
+      </div>
+      <div
+        className="absolute w-[612px] h-[515px] left-[450px] top-[274px] 
+        bg-[#CCCCCC]/[0.2] rounded-[30px]"
+      >
+        <div className="absolute w-[449px] h-[63px] bg-white left-[80px] top-[100px]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
